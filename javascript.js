ff9fe4d75d89bebf0b7f2a67502e6c225dbe7bcb
hanapinpaul <164150148+hanapinpaul@users.noreply.github.com>
2024-04-27 08:20:15 +0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,34 @@
+let loginForm = document.getElementById("Portal Registration Form");
+// SELECTING STUDENT
+console.log("Selecting Student");
+let selectedStudent = ["New Student", "Old Student"];
+console.log(selectedStudent[1]);
+// STUDENT'S NAME
+console.log("Student's Full Name");
+let fullName = {
+ firstName: "Paul Angelo",
+ middleName: "Florida",
+ lastName: "Hanapin"
+};
+console.log(fullName);
+// AGE
+console.log("Student's Age");
+let Age = "22";
+console.log(Age); 
+// SELECTING GENDER
+console.log("Selecting Gender");
+let male = true;
+let female = false;
+let selectedGender = ["Male", "Female"];
+console.log(selectedGender[0]);
+// INFORMATION
+console.log("Student's Information");
+let information = {
+    address: "blk 31 lt 10 cashew st. phase 3, langgam spl",
+    contactNumber: "09925149430",
+    email: "hanapinpaul@gmail.com"
+   };
+console.log(information);
+// USERNAME & PASSWORD
+let myUsername = document.getElementById('userName');
+console.log(myUsername);
